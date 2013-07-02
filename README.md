1cce39e08a18bc225bb0be51e1bdc3cba548b320
Laurence Joseph Smith <laurence@sourceless.org>
2013-07-03 00:18:10 +0100

    scraper updated, works, barely

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 Currently a problem: only works when links are relative paths. Might fix but this project isn't exactly long term.
+
+Problem 2: my rushed attempt at getting them to go into nice files/folders didn't work. Preserved in filename instead.
+
+# To use
+
+Go into the directory you want to scrape the site to and then `python <path-to-scraper.py>`
--- scraper.py
+++ scraper.py
@@ -1,17 +1,47 @@
 import mapper
+import requests
 import os
+import os.path
 import sys
-from collection import defaultdict
+import json
+import codecs
 
 
 def scrape_recursively(url, dir_=os.getcwd()):
     print "Scraping links {}, this could take some time...".format(url)
     link_list = mapper.map_site(url)
-    print "Done, mapping..."
-    link_list = [link.strip(url) for link in link_list]
+    print "Done. Scraping pages, this will take a litte while..."
+    
+    for link in link_list:
+        print "Scraping {}".format(link)
+        page = requests.get(link)
+        file_loc = os.path.join(dir_, link.strip(url))
+        #ensure_dirs(file_loc)
 
-    link_map = defaultdict
+        #with codecs.open(file_loc + '`.html', 'w', 'utf-8') as f:
+        with codecs.open(link + '.html', 'w', 'utf-8') as f:
+            f.write(page.text)
 
+        # dirty, hacky, horrible, please never do this
+        try:
+            print "Trying to get page JSON"
+            link_json = link + '.json'
+            page_json = json.loads(requests.get(link_json).text)
+            #file_loc_json = os.path.join(dir_, link_json.strip(url))
+            #ensure_dirs(file_loc_json)
+
+            #with codecs.open(file_loc_json + '.html', 'w', 'utf-8') as f:
+            with codecs.open(link_json, 'w', 'utf-8') as f:
+                f.write(json.dumps(page_json))
+            print "Success"
+        except Exception:
+            print "Failed"
+            continue
+
+def ensure_dirs(f):
+    d = os.path.dirname(f)
+    if not os.path.exists(d):
+        os.makedirs(d)
 
 
 if __name__ == '__main__':
