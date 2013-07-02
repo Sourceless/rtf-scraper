5cca70e4a589b8ff5fca706eb7fb5a9f14aea771
Laurence Joseph Smith <laurence@sourceless.org>
2013-07-02 23:15:27 +0100

    get recursive link following working

--- mapper.py
+++ mapper.py
@@ -1,6 +1,25 @@
 import requests
 from BeautifulSoup import BeautifulSoup
-import os.path
+from collections import deque
+
+
+def map_site(url):
+    '''Find all the linked-to pages of a site and return a list of them'''
+    not_searched = [url]
+    searched = []
+
+    while len(not_searched):
+        search_url = not_searched.pop()
+        if search_url not in searched:
+            print "Searching {}".format(search_url)
+            not_searched.extend(scrape_links(url, search_url))
+        else:
+            print "Skipping {}, already scraped".format(search_url)
+            continue
+        searched.append(search_url) 
+
+    return not_searched, searched
+
 
 def scrape_links(root, url):
     '''Scrape an url for links, return a list of urls'''
@@ -8,7 +27,9 @@ def scrape_links(root, url):
     soup = BeautifulSoup(page.text)
 
     anchors = soup.findAll('a', href=True)
-    anchors = [rootify(root, anchor.attrs[0][1]) for anchor in anchors]
+    anchors = [dict(anchor.attrs)['href'] for anchor in anchors]
+    anchors = [anchor for anchor in anchors if anchor != None]
+    anchors = [rootify(root, anchor) for anchor in anchors if anchor.startswith('/')]
 
     return anchors
 
@@ -20,5 +41,5 @@ def rootify(root, url):
 
 
 if __name__ == '__main__':
-    print scrape_links("http://www.restorethefourth.net/", "http://www.restorethefourth.net/")
+    print map_site("http://www.restorethefourth.net/")
 
