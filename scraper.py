921191691958d68f47cf28ede6976d8e0294bbc0
Laurence Joseph Smith <laurence@sourceless.org>
2013-07-03 01:22:30 +0100

    directory and index fix

--- scraper.py
+++ scraper.py
@@ -10,16 +10,17 @@ import codecs
 def scrape_recursively(url, dir_=os.getcwd()):
     print "Scraping links {}, this could take some time...".format(url)
     link_list = mapper.map_site(url)
-    print "Done. Scraping pages, this will take a litte while..."
+    print "Done. Scraping pages, this will take a little while..."
     
     for link in link_list:
         print "Scraping {}".format(link)
         page = requests.get(link)
-        file_loc = os.path.join(dir_, link.strip(url))
-        #ensure_dirs(file_loc)
+        file_loc = os.path.join(dir_, link[len(url):].strip('/')) + '.html'
+        if link == url:
+            file_loc = './index.html'
+        ensure_dirs(file_loc)
 
-        #with codecs.open(file_loc + '`.html', 'w', 'utf-8') as f:
-        with codecs.open(link + '.html', 'w', 'utf-8') as f:
+        with codecs.open(file_loc, 'w', 'utf-8') as f:
             f.write(page.text)
 
         # dirty, hacky, horrible, please never do this
@@ -27,11 +28,12 @@ def scrape_recursively(url, dir_=os.getcwd()):
             print "Trying to get page JSON"
             link_json = link + '.json'
             page_json = json.loads(requests.get(link_json).text)
-            #file_loc_json = os.path.join(dir_, link_json.strip(url))
-            #ensure_dirs(file_loc_json)
+            file_loc_json = os.path.join(dir_, link_json[len(url):].strip('/'))
+            if link == url:
+                file_loc_json = './index.json'
+            ensure_dirs(file_loc_json)
 
-            #with codecs.open(file_loc_json + '.html', 'w', 'utf-8') as f:
-            with codecs.open(link_json, 'w', 'utf-8') as f:
+            with codecs.open(file_loc_json, 'w', 'utf-8') as f:
                 f.write(json.dumps(page_json))
             print "Success"
         except Exception:
